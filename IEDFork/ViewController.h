d4ceee55abab347ad62c059df0f0c8a12abd5b54
Samuel Seng <djsamseng@gmail.com>
2014-10-26 19:36:10 -0400

    Merged changes to include voice commands

--- IEDFork/ViewController.h
+++ IEDFork/ViewController.h
@@ -9,8 +9,21 @@
 #import <UIKit/UIKit.h>
 #import "BLE.h"
 
-@interface ViewController : UIViewController <BLEDelegate>
+
+#import <Slt/Slt.h>
+#import <OpenEars/FliteController.h>
+
+#import <OpenEars/LanguageModelGenerator.h>
 
 
+#import <OpenEars/PocketsphinxController.h>
+#import <OpenEars/AcousticModel.h>
+
+#import <OpenEars/OpenEarsEventsObserver.h>
+
+@interface ViewController : UIViewController <BLEDelegate, OpenEarsEventsObserverDelegate>     @property (strong, nonatomic) FliteController *fliteController;
+    @property (strong, nonatomic) Slt *slt;
+    @property (strong, nonatomic) PocketsphinxController *pocketsphinxController;
+    @property (strong, nonatomic) OpenEarsEventsObserver *openEarsEventsObserver;
 @end
 
--- IEDFork/ViewController.m
+++ IEDFork/ViewController.m
@@ -9,9 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+
+
+
 @property (strong, nonatomic) IBOutlet UITextView *textReceived;
 @property (strong, nonatomic) IBOutlet UIButton *connectButton;
 @property (strong, nonatomic) BLE *bleShield;
+
 
 @end
 
@@ -19,9 +23,44 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.textReceived.text=NULL;
+    
     self.bleShield = [[BLE alloc] init];
     [self.bleShield controlSetup];
     self.bleShield.delegate = self;
+    
+    
+    LanguageModelGenerator *lmGenerator = [[LanguageModelGenerator alloc] init];
+    NSArray* words = [NSArray arrayWithObjects:@"YES", @"CONNECT", nil];
+    NSString*name = @"recognitionwords";
+    NSError *err = [lmGenerator generateLanguageModelFromArray:words withFilesNamed:name forAcousticModelAtPath:[AcousticModel pathToModel:@"AcousticModelEnglish"]];
+    
+    NSDictionary *languageGeneratorResults = nil;
+    
+    NSString *lmPath = nil;
+    NSString *dicPath = nil;
+    
+    if([err code] == noErr) {
+        
+        languageGeneratorResults = [err userInfo];
+		
+        lmPath = [languageGeneratorResults objectForKey:@"LMPath"];
+        dicPath = [languageGeneratorResults objectForKey:@"DictionaryPath"];
+		
+    } else {
+        NSLog(@"Error: %@",[err localizedDescription]);
+    }
+    
+    
+    [self.openEars setDelegate:self];
+    
+    [self.pocket startListeningWithLanguageModelAtPath:lmPath dictionaryAtPath:dicPath acousticModelAtPath:[AcousticModel pathToModel:@"AcousticModelEnglish"] languageModelIsJSGF:NO];
+    
+  
+    
+    [self.flite say:@"For instructions say yes" withVoice:self.s];
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -89,6 +128,93 @@ NSTimer *rssiTimer;
     
 }
 
+- (FliteController *)flite {  //controls the voice
+	if (self.fliteController == nil) {
+		self.fliteController = [[FliteController alloc] init];
+	}
+	return self.fliteController;
+}
+
+- (Slt *)s { //voice, might want to change
+	if (self.slt == nil) {
+		self.slt = [[Slt alloc] init];
+	}
+	return self.slt;
+}
+- (PocketsphinxController *)pocket{
+	if (self.pocketsphinxController == nil) {
+		self.pocketsphinxController = [[PocketsphinxController alloc] init];
+	}
+	return self.pocketsphinxController;
+}
+- (OpenEarsEventsObserver *)openEars {
+	if (self.openEarsEventsObserver == nil) {
+		self.openEarsEventsObserver = [[OpenEarsEventsObserver alloc] init];
+	}
+	return self.openEarsEventsObserver;
+}
+
+- (void) pocketsphinxDidReceiveHypothesis:(NSString *)hypothesis recognitionScore:(NSString *)recognitionScore utteranceID:(NSString *)utteranceID {
+    
+	NSLog(@"The received hypothesis is %@ with a score of %@ and an ID of %@", hypothesis, recognitionScore, utteranceID);
+    if([hypothesis isEqualToString:@"YES"])
+    {
+        [self.flite say:@"Welcome to Fork  It. Forblue tooth connection please say the command: Connect" withVoice:self.s];
+        self.textReceived.text= @"You said Yes";
+        
+        
+    }
+    if([hypothesis isEqualToString:@"CONNECT"]){
+        
+        [self bleConnectPressed:nil];
+        self.textReceived.text= @"You said Connect";
+    }
+}
+
+- (void) pocketsphinxDidStartCalibration {
+	NSLog(@"Pocketsphinx calibration has started.");
+}
+
+- (void) pocketsphinxDidCompleteCalibration {
+	NSLog(@"Pocketsphinx calibration is complete.");
+}
+
+- (void) pocketsphinxDidStartListening {
+	NSLog(@"Pocketsphinx is now listening.");
+}
+
+- (void) pocketsphinxDidDetectSpeech {
+    
+	NSLog(@"Pocketsphinx has detected speech.");
+    
+}
+
+- (void) pocketsphinxDidDetectFinishedSpeech {
+	NSLog(@"Pocketsphinx has detected a period of silence, concluding an utterance.");
+}
+
+- (void) pocketsphinxDidStopListening {
+	NSLog(@"Pocketsphinx has stopped listening.");
+}
+
+- (void) pocketsphinxDidSuspendRecognition {
+	NSLog(@"Pocketsphinx has suspended recognition.");
+}
+
+- (void) pocketsphinxDidResumeRecognition {
+	NSLog(@"Pocketsphinx has resumed recognition.");
+}
+
+- (void) pocketsphinxDidChangeLanguageModelToFile:(NSString *)newLanguageModelPathAsString andDictionary:(NSString *)newDictionaryPathAsString {
+	NSLog(@"Pocketsphinx is now using the following language model: \n%@ and the following dictionary: %@",newLanguageModelPathAsString,newDictionaryPathAsString);
+}
+
+- (void) pocketSphinxContinuousSetupDidFail { // This can let you know that something went wrong with the recognition loop startup. Turn on OPENEARSLOGGING to learn why.
+	NSLog(@"Setting up the continuous recognition loop has failed for some reason, please turn on OpenEarsLogging to learn more.");
+}
+- (void) testRecognitionCompleted {
+	NSLog(@"A test file that was submitted for recognition is now complete.");
+}
 
 
 @end
